c706a061bdc8676452e2d2af6392bdf13b6cc529
ppyyyhhh <hpy0531@gmail.com>
2020-06-20 03:57:27 -0700

    Building the basic blocks

--- Learning R programming with swirl.R
+++ Learning R programming with swirl.R
@@ -1,7 +1,27 @@
 5+7
 x<-5+7
 x
-y <- x - 3 #Now, store the result of x - 3 in a new variable called y.
-y # What is the value of y? Type y to find out.
-z <- c(1.1, 9, 3.14) #To create a vector containing the numbers 1.1, 9, and 3.14, | type c(1.1, 9, 3.14). Try it now and store the result in a variable called z.
-?c # Anytime you have questions about a particular function, you can access R's built-in help files via the `?` command. For example, if you want more information on the c() function,
+
+y <- x - 3      #Now, store the result of x - 3 in a new variable called y.
+
+y      # What is the value of y? Type y to find out.
+
+z <- c(1.1, 9, 3.14)    #To create a vector containing the numbers 1.1, 9, and 3.14, | type c(1.1, 9, 3.14). Try it now and store the result in a variable called z.
+
+?c  # Anytime you have questions about a particular function, you can access R's built-in help files via the `?` command. For example, if you want more information on the c() function,
+
+z   #Type z to view its contents. Notice that there are no commas separating the values in the output.
+
+c(z, 555, z)    #You can combine vectors to make a new vector. Create a new vector that contains z, 555, then z again in that order. Don't assign this vector to a new variable, so that we can just see the result immediately.
+
+z * 2 + 100     #Numeric vectors can be used in arithmetic expressions. Type the following to see what happens: z * 2 + 100.
+# First, R multiplied each of the three elements in z by 2. Then it added 100 to each element to get the result you see above.
+
+my_sqrt <- sqrt(z-1) #Take the square root of z - 1 and assign it to a new variable called my_sqrt.
+#  a vector of length 3
+
+print(my_sqrt)
+
+my_sqrt     #Just type my_sqrt and press Enter to view its value.
+
+
