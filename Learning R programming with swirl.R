e897406a967083f2354725468ff97fcce276d3db
ppyyyhhh <hpy0531@gmail.com>
2020-06-20 03:57:06 -0700

    Building the basic blocks

---
+++ Learning R programming with swirl.R
@@ -0,0 +1,7 @@
+5+7
+x<-5+7
+x
+y <- x - 3 #Now, store the result of x - 3 in a new variable called y.
+y # What is the value of y? Type y to find out.
+z <- c(1.1, 9, 3.14) #To create a vector containing the numbers 1.1, 9, and 3.14, | type c(1.1, 9, 3.14). Try it now and store the result in a variable called z.
+?c # Anytime you have questions about a particular function, you can access R's built-in help files via the `?` command. For example, if you want more information on the c() function,
