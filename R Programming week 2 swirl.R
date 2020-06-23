5ce433986e90a9910c86a8700974373cd03627bc
ppyyyhhh <hpy0531@gmail.com>
2020-06-22 21:02:37 -0700

    Week 2 lesson 1.

--- R Programming week 2 swirl.R
+++ R Programming week 2 swirl.R
@@ -2,3 +2,45 @@
 #swirl lessons 1: Logic
 
 require(swirl())
+swirl()
+Happy
+1
+8
+require(swirl)
+swirl()
+Happy
+1
+
+TRUE == TRUE
+FALSE == FALSE
+(FALSE == TRUE) == FALSE
+6== 7
+6< 7
+10 <= 10
+3
+4
+5 != 7
+5 == 7
+!(5==7)
+24
+4
+2
+4
+1
+FALSE & FALSE
+TRUE & c(TRUE, FALSE, FALSE)
+TRUE && c(TRUE, FALSE, FALSE)
+TRUE | c(TRUE, FALSE,FALSE)
+TRUE || c(TRUE, FALSE, FALSE)
+5> 8 || 6 != 8 && 4 >3.9
+4
+isTRUE(6>4)
+5
+identical('twins','twins')
+4
+xor(5 ==6, !FALSE)
+ints
+ints>5
+all(ints>0)
+2
+
